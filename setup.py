b50659eef80727cfc7d1df8d1148ccaf922e598d
NuggetOfficial <tvdwln@gmail.com>
2023-08-08 10:55:48 +0200

    additional information provided in setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, Extension
+from setuptools import setup
 
 setup(
     name='configlib',
@@ -7,5 +7,17 @@ setup(
     description='package to neetly organise your global config parameters',
     author='Tom van der Wielen',
     author_email='vdwielen@strw.leidenuniv.nl',
-    version='0.1',
+    version='0.0.1.a0',
+    url='https://github.com/NuggetOfficial/configlib',
+    install_requires=['PyYAML>=6.0.1'],
+    python_requires='>=3.11.*',
+    license='gpl-3.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Project Management :: Configuration ',
+        'License :: OSI Approved :: GNU License',
+        'Programming Language :: Python :: 3.11',
+    ],
+    keywords='configuration project'
 )

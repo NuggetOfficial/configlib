8835fb96bed4dc428b40f2aba867fb9f4608eff8
NuggetOfficial <tvdwln@gmail.com>
2023-08-08 11:08:48 +0200

    might cause circular import

--- configlib/__init__.py
+++
@@ -1,9 +0,0 @@
-from configlib import UnitTests
-# do tests
-if not UnitTests():
-    raise Exception('package critically failed unit tests! please contact me, vdwielen@strw.leidenuniv.nl or open an issue on the github repository!')
-# Test not for import
-del UnitTests
-
-# currently intented imports
-from configlib import Config, FileConfig, ConfigIO

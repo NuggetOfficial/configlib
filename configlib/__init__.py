7a8533d3dd92f5b1a87d42a8267d4624b166f9e4
NuggetOfficial <tvdwln@gmail.com>
2023-08-13 18:48:24 +0200

    added import for new object and errors

--- configlib/__init__.py
+++ configlib/__init__.py
@@ -1,2 +1,4 @@
 from .configlib import Config, FileConfig
 from .configlib import ConfigIO
+from .configlib import ArgumentParserWithFallback
+from .configlib import NameError, AliasUnavailableError, DefaultNotRegisteredError

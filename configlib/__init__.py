38a2e87355e710bbef0f5ecc79b03df0bc476dc6
NuggetOfficial <tvdwln@gmail.com>
2023-08-20 11:55:35 +0200

    added ModelConfig

--- configlib/__init__.py
+++ configlib/__init__.py
@@ -1,4 +1,4 @@
-from .configlib import Config, BaseConfig, FileConfig
+from .configlib import Config, BaseConfig, FileConfig, ModelConfig
 from .configlib import ConfigIO, ConfigFormatter
 from .configlib import ArgumentParserWithFallback
 from .configlib import NameError, AliasUnavailableError, DefaultNotRegisteredError
